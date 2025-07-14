9de44c71a7ba90b3b4b8840d1e0fa03eab836047
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-14 10:08:05 +0600

    string js updated

--- string.js
+++ string.js
@@ -1,7 +1,16 @@
-let country = "Bangladesh";
+let country = "Bangla desh";
 let division = "odisha";
 let district = `B-baria`;
 const thana = new String("Jatrabari");
 
 console.log(typeof thana);
 console.log(thana);
+console.log(country.length);
+console.log(thana.length);
+console.log(thana[2]);
+
+//  Numbers
+
+const number = [11, 22, 33, 44, 55, 66, 77, 88];
+console.log(number.length);
+console.log(number[2]);
