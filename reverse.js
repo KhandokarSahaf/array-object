086aa9c9b909abe314be456c44e8101877ccbe14
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-18 11:27:23 +0600

    2 more different way of reversee js

--- reverse.js
+++ reverse.js
@@ -5,3 +5,20 @@ for (const letter of foodName) {
   reverse = letter + reverse;
 }
 console.log(reverse);
+
+// 2nd option
+const fdName = "Khandokar Sahaf";
+let rev = "";
+for (i = 0; i < fdName.length; i++) {
+  //   console.log(i);
+  //   console.log(fdName[i]);
+  const letter = fdName[i];
+  rev = letter + rev;
+}
+console.log(rev);
+
+// 3rd and shorter way
+
+const placeName = "Dhaka is the capital of Bangladesh";
+const reversed = placeName.split("").reverse().join("");
+console.log(reversed);
