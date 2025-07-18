d47e6d35a99035325bd2394783b7bd7fd7d00ee2
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-18 11:35:39 +0600

    in case1 i tried to revission case js

---
+++ case1.js
@@ -0,0 +1,10 @@
+const fristName = "sahaf";
+const realName = "  SaHaF  ";
+
+if (
+  fristName.toLocaleLowerCase().trim() === realName.toLocaleLowerCase().trim()
+) {
+  console.log("your name is ok");
+} else {
+  console.log("no");
+}
