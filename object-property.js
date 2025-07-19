6456e80d0df2463a26117e7a4fbc80e66728e64f
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-19 21:54:05 +0600

    i declare object and call its indivisual property

--- object-property.js
+++ object-property.js
@@ -8,3 +8,9 @@ const ahaf = {
 
 console.log(ahaf.age);
 console.log(ahaf["my hobby"]);
+// we also can store the value of a property into a new variable and call the new variable for console.log or  simply change their value by store new value into 2nd declared variable without changing the value of my real value
+
+ahaf.age = 23;
+const updateAge = ahaf.age;
+
+console.log(updateAge);
