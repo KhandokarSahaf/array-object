47a28102a5f9800aa6e256c001a3810f36afeb74
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-19 21:45:11 +0600

    i declare object and call its indivisual property

---
+++ object-property.js
@@ -0,0 +1,10 @@
+const ahaf = {
+  fullName: "Khandokar Saeed Al Sahaf",
+  age: 22,
+  permanentAdd: "Dhaka",
+  profession: "web developer",
+  "my hobby": ["reading books", "sleeping", "talking"],
+};
+
+console.log(ahaf.age);
+console.log(ahaf["my hobby"]);
