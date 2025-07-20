a01c963fa521b2fd07a59fb445951c9b62095d8b
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-20 13:31:12 +0600

    object declaretion

---
+++ object.js
@@ -0,0 +1,10 @@
+const id = {
+  name: "Sahaf",
+  num: 01608975686,
+  destination: ["Dhaka", "Cumiila", "Khustiya", "Rajshai", "Kuakata"],
+  "School name": "Jatrabari Ideal School",
+};
+
+console.log(id.name);
+
+console.log(id["School name"]);
