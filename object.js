b1ffdc4c0edb8b085df502b6bdf3d01ec1d1d771
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-20 13:38:33 +0600

    string ar object ar value practice korlam

--- object.js
+++ object.js
@@ -1,6 +1,6 @@
 const id = {
   name: "Sahaf",
-  num: 01608975686,
+  num: "01608975686",
   destination: ["Dhaka", "Cumiila", "Khustiya", "Rajshai", "Kuakata"],
   "School name": "Jatrabari Ideal School",
 };
---
+++ value.js
@@ -0,0 +1,14 @@
+const sahaf = {
+  fristName: "Khandokar",
+  lastName: "Sahaf",
+  "Phone Number": "01068975686",
+  profession: "developer",
+};
+
+sahaf["Phone Number"] = "01687157906";
+// const updateNumber = sahaf["Phone Number"];
+sahaf.profession = "Web Developer";
+
+console.log(sahaf["Phone Number"]);
+// console.log(updateNumber);
+console.log(sahaf);
