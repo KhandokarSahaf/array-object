b5be08df625dabe9f1ec563fd587a43facaf60e4
KhandokarSahaf <khandokarsahaf3@gmail.com>
2025-07-21 16:28:57 +0600

    declare and create new object

---
+++ declare.js
@@ -0,0 +1,10 @@
+const pen = {
+  brand: "Matador",
+  price: 5,
+  color: "black",
+};
+const pencil = new Object();
+console.log(pencil);
+
+const rubber = Object.create({});
+console.log(rubber);
